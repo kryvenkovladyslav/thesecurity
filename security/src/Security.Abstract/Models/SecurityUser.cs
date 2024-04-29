cb934013d06aa1358e46d340015cd4f501af32c2
DESKTOP-38Q3HR9\Vladyslav Kryvenko <onekryvenko.vladyslav@gmail.com>
2024-04-29 23:01:51 +0300

    Added basic structure for implementing the user store

---
+++ security/src/Security.Abstract/Models/SecurityUser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+
+namespace Security.Abstract
+{
+    /// <summary>
+    /// This class represents a user in the Security System
+    /// </summary>
+    /// <typeparam name="TIdentifier">Represents the identifier of a user</typeparam>
+    public abstract class SecurityUser<TIdentifier> : IEquatable<SecurityUser<TIdentifier>>
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        /// <summary>
+        /// Represent the identifier of a user
+        /// </summary>
+        public TIdentifier ID { get; init; }
+
+        /// <summary>
+        /// Represents a name of a user
+        /// </summary>
+        public string UserName { get; init; }
+
+        /// <summary>
+        /// Represents a normalized name of a user
+        /// </summary>
+        public string NormalizedUserName { get; init; }
+
+        /// <summary>
+        /// Compares two users by identifier
+        /// </summary>
+        /// <param name="obj">The object will be compared</param>
+        /// <returns>True if the object represents a user with a special identifier, otherwise False</returns>
+        public override bool Equals(object obj)
+        {
+            var user = obj as SecurityUser<TIdentifier>;
+
+            return user != null ? this.Equals(user) : false;
+        }
+
+        /// <summary>
+        /// Returns a hash code of the instance
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return this.ID.GetHashCode();
+        }
+
+        /// <summary>
+        /// Compares two users by identifier
+        /// </summary>
+        /// <param name="other">The user will be compared</param>
+        /// <returns>True if two object have the same identifier, otherwise False</returns>
+        public bool Equals(SecurityUser<TIdentifier> other)
+        {
+            return this.ID.Equals(other.ID) && this.NormalizedUserName == other.NormalizedUserName;
+        }
+
+        /// <summary>
+        /// Provides a string representation for the current user
+        /// </summary>
+        /// <returns>A string representing a user state</returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder
+                .Append("{UserID:\t")
+                .Append(this.ID)
+                .Append(",\t")
+                .Append("UserName:\t")
+                .Append(this.UserName)
+                .Append("}");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ security/src/Security.DataAccess/SecurityDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Security.Abstract;
+using System;
+
+namespace Security.DataAccess
+{
+    public abstract class SecurityDatabaseContext<TUser, TIdentifier> : DbContext
+        where TUser : SecurityUser<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        public DbSet<TUser> Users { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
