1ff5e4b3ad220891e3ccf8aac6e5b69ffa43e6ec
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-06-30 11:05:18 +0300

    Implemented a Password Hasher

---
+++ security/src/Security/SecurityPasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Security.Abstract;
+using System;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Security
+{
+    public class SecurityPasswordHasher<TUser, TIdentifier> : IPasswordHasher<TUser>
+        where TUser : SecurityUser<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        public virtual string HashPassword(TUser user, string password)
+        {
+            var convertedIdentifier = TypeDescriptor.GetConverter(typeof(string)).ConvertToInvariantString(user.ID);
+
+            var hashAlgorithm = new HMACSHA256(Encoding.UTF8.GetBytes(convertedIdentifier));
+            return BitConverter.ToString(hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(password)));
+        }
+
+        public virtual PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+        {
+            return this.HashPassword(user, providedPassword) == hashedPassword ? 
+                PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+        }
+    }
+}
