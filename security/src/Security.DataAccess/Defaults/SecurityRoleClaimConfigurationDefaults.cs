cb809b4450ca7bacb6ed65392a64f5268df1adf2
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-07-22 20:49:03 +0300

    Added RoleClaim defaults

---
+++ security/src/Security.DataAccess/Defaults/SecurityRoleClaimConfigurationDefaults.cs
@@ -0,0 +1,15 @@
+﻿namespace Security.DataAccess
+{
+    internal static class SecurityRoleClaimConfigurationDefaults
+    {
+        public static string TableName { get; } = "SecurityRoleClaim";
+
+        public static string IdentifierColumnName { get; } = "ID";
+
+        public static string RoleIdentifierColumnName { get; } = "RoleID";
+
+        public static string ClaimTypeColumnName { get; } = "Type";
+
+        public static string ClaimValueColumnName { get; } = "Value";
+    }
+}
