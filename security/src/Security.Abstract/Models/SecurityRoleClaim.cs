34205c7913e4aa289c8eb39ff63b9e66c92ab45e
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-07-22 20:23:40 +0300

    Refactored the RoleClaim model

--- security/src/Security.Abstract/Models/SecurityRoleClaim.cs
+++ security/src/Security.Abstract/Models/SecurityRoleClaim.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Claims;
 using System.Text;
 
 namespace Security.Abstract
@@ -12,19 +11,14 @@ namespace Security.Abstract
         where TIdentifier : IEquatable<TIdentifier>
     {
         /// <summary>
-        /// Gets the of the primary key of the role associated with this claim
+        /// Gets the primary key of the role
         /// </summary>
         public virtual TIdentifier RoleID { get; init; }
 
         /// <summary>
-        /// Gets or sets the claim type for this claim
+        /// Gets the primary key of the claim 
         /// </summary>
-        public virtual string Type { get; set; }
-
-        /// <summary>
-        /// Gets or sets the claim value for this claim
-        /// </summary>
-        public virtual string Value { get; set; }
+        public virtual TIdentifier ClaimID { get; init; }
 
         /// <summary>
         /// Compares two role-claim by identifier
@@ -67,38 +61,13 @@ namespace Security.Abstract
 
             builder
                 .Append("{ClaimID:\t")
-                .Append(this.ID)
+                .Append(this.ClaimID)
                 .Append(",\t")
                 .Append("{RoleID:\t")
                 .Append(this.RoleID)
-                .Append(",\t")
-                .Append("Type:\t")
-                .Append(this.Type)
-                .Append(",\t")
-                .Append("Value:\t")
-                .Append(this.Value)
                 .Append("}");
 
             return builder.ToString();
-        }
-
-        /// <summary>
-        /// Constructs a new claim with the type and value
-        /// </summary>
-        /// <returns>The <see cref="Claim"/> that was produced</returns>
-        public virtual Claim ToClaim()
-        {
-            return new Claim(this.Type, this.Value);
-        }
-
-        /// <summary>
-        /// Initializes by copying ClaimType and ClaimValue from the other claim
-        /// </summary>
-        /// <param name="other">The claim to initialize from</param>
-        public virtual void InitializeFromClaim(Claim other)
-        {
-            this.Type = other?.Type;
-            this.Value = other?.Value;
         }
     }
 }
