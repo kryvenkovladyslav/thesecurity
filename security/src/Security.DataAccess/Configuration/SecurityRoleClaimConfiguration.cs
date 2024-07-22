821fd53fd44f1b02e27b7d64381397ca9b6c3064
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-07-22 20:32:12 +0300

    Added role table configuration

--- security/src/Security.DataAccess/Configuration/SecurityRoleClaimConfiguration.cs
+++ security/src/Security.DataAccess/Configuration/SecurityRoleClaimConfiguration.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Security.Abstract;
+using System;
 
-namespace Security.DataAccess.Configuration
+namespace Security.DataAccess
 {
-    internal class SecurityRoleClaimConfiguration
+    internal sealed class SecurityRoleClaimConfiguration<TSecurityRole,TIdentifier> : IEntityTypeConfiguration<TSecurityRole>
+        where TIdentifier : IEquatable<TIdentifier>
+        where TSecurityRole : SecurityRole<TIdentifier>
     {
+        public void Configure(EntityTypeBuilder<TSecurityRole> builder)
+        {
+            var roleTable = builder.ToTable(SecurityRoleConfigurationDefaults.TableName);
+
+            roleTable.HasKey(role => role.ID);
+
+            roleTable
+                .Property(role => role.ID)
+                .HasColumnName(SecurityRoleConfigurationDefaults.IdentifierColumnName)
+                .IsRequired();
+
+            roleTable
+                .Property(role => role.Name)
+                .HasColumnName(SecurityRoleConfigurationDefaults.NameColumnName)
+                .IsRequired();
+
+            roleTable
+                .Property(role => role.NormalizedName)
+                .HasColumnName(SecurityRoleConfigurationDefaults.NormalizedNameColumnName)
+                .IsRequired();
+
+            roleTable
+                .Property(role => role.ConcurrencyStamp)
+                .HasColumnName(SecurityRoleConfigurationDefaults.ConcurrencyStampColumnName)
+                .IsRequired();
+        }
     }
 }
---
+++ security/src/Security.DataAccess/Defaults/SecurityRoleConfigurationDefaults.cs
@@ -0,0 +1,15 @@
+﻿namespace Security.DataAccess
+{
+    internal static class SecurityRoleConfigurationDefaults
+    {
+        public static string TableName { get; } = "SecurityRole";
+
+        public static string IdentifierColumnName { get; } = "ID";
+
+        public static string NameColumnName { get; } = "Name";
+
+        public static string NormalizedNameColumnName { get; } = "NormalizedName";
+
+        public static string ConcurrencyStampColumnName { get; } = "ConcurrencyStamp";
+    }
+}
