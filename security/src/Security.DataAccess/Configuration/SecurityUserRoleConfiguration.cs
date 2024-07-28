2bf9e59752fdecf959102460a3b82abedf1dcada
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-07-28 16:47:53 +0300

    Added SecurityUserRoleConfiguration

--- security/src/Security.DataAccess/Configuration/SecurityUserRoleConfiguration.cs
+++ security/src/Security.DataAccess/Configuration/SecurityUserRoleConfiguration.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Security.Abstract;
+using System;
 
 namespace Security.DataAccess.Configuration
 {
-    internal class SecurityUserRoleConfiguration
+    internal sealed class SecurityUserRoleConfiguration<TSecurityUser, TSecurityRole, TSecurityUserRole, TIdentifier> : IEntityTypeConfiguration<TSecurityUserRole>
+        where TIdentifier : IEquatable<TIdentifier>
+        where TSecurityUser : SecurityUser<TIdentifier>
+        where TSecurityRole : SecurityRole<TIdentifier>
+        where TSecurityUserRole : SecurityUserRole<TIdentifier>
     {
+        public void Configure(EntityTypeBuilder<TSecurityUserRole> builder)
+        {
+            var securityUserRoleTable = builder.ToTable(SecurityUserRoleConfigurationDefaults.TableName);
+
+            securityUserRoleTable.HasKey(claim => claim.ID);
+
+            securityUserRoleTable
+                .Property(userRole => userRole.ID)
+                .HasColumnName(SecurityUserRoleConfigurationDefaults.IdentifierColumnName)
+                .IsRequired();
+
+            securityUserRoleTable
+                .Property(userRole => userRole.UserID)
+                .HasColumnName(SecurityUserRoleConfigurationDefaults.UserIdentifierColumnName)
+                .IsRequired();
+
+            securityUserRoleTable
+                .Property(userRole => userRole.RoleID)
+                .HasColumnName(SecurityUserRoleConfigurationDefaults.RoleIdentifierColumnName)
+                .IsRequired();
+
+            securityUserRoleTable.HasOne<TSecurityUser>().WithMany()
+                .HasForeignKey(userRole => userRole.UserID).HasPrincipalKey(user => user.ID);
+
+            securityUserRoleTable.HasOne<TSecurityRole>().WithMany()
+                .HasForeignKey(userRole => userRole.RoleID).HasPrincipalKey(role => role.ID);
+        }
     }
 }
---
+++ security/src/Security.DataAccess/Defaults/SecurityUserRoleConfigurationDefaults.cs
@@ -0,0 +1,13 @@
+﻿namespace Security.DataAccess
+{
+    internal static class SecurityUserRoleConfigurationDefaults
+    {
+        public static string TableName { get; } = "SecurityUserRole";
+
+        public static string IdentifierColumnName { get; } = "ID";
+
+        public static string UserIdentifierColumnName { get; } = "UserID";
+
+        public static string RoleIdentifierColumnName { get; } = "RoleID";
+    }
+}
